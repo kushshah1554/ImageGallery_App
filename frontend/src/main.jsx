fc43d0d46e74db67d0a3878b05aeb801c39b0ce0
kush shah <skush1554@gmail.com>
2025-11-25 17:46:52 +0545

    added gitignore file and change defaults baseURl of axios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,7 @@ import App from './App.jsx'
 import TokenProvider from './TokenProvider.jsx'
 import axios from 'axios'
 
-axios.defaults.baseURL =
-  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3000";
+axios.defaults.baseURL =import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3000";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
